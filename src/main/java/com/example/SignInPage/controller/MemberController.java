a713c64bd2e52a1261aca54a0dfd0cd506196057
Shin Ho Geun <shg9611@naver.com>
2025-06-24 17:21:20 +0900

    add repository

--- src/main/java/com/example/SignInPage/controller/MemberController.java
+++ src/main/java/com/example/SignInPage/controller/MemberController.java
@@ -2,12 +2,17 @@ package com.example.SignInPage.controller;
 
 import com.example.SignInPage.dto.JoinEntity;
 import com.example.SignInPage.dto.JoinForm;
+import com.example.SignInPage.repository.JoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
+
+    @Autowired
+    private JoinRepository joinRepository;
 
     @GetMapping("/signup")
     public String signUpPage(){
@@ -19,8 +24,11 @@ public class MemberController {
 
         JoinEntity joinEntity=joinForm.toEntity();
 
+        JoinEntity saveEntity=joinRepository.save(joinEntity);
+
         System.out.println(joinForm);
         System.out.println(joinEntity);
+        System.out.println(saveEntity);
         return"";
     }
 }
---
+++ src/main/java/com/example/SignInPage/repository/JoinRepository.java
@@ -0,0 +1,7 @@
+package com.example.SignInPage.repository;
+
+import com.example.SignInPage.dto.JoinEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface JoinRepository extends CrudRepository<JoinEntity,Long> {
+}
