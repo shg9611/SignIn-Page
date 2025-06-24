134d5da775443da96615ec189e9ca2fd8f2ba53e
Shin Ho Geun <shg9611@naver.com>
2025-06-24 15:51:22 +0900

    first commit

---
+++ src/main/java/com/example/SignInPage/SignInPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.SignInPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignInPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignInPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SignInPage/SignInPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SignInPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignInPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
